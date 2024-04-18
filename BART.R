ff2c2fc65a7e4a3fd04a96e79539533da1d12dd2
halljc76 <jchall076@outlook.com>
2024-04-18 19:46:27 -0400

    Bart Simpson Grew Up

--- BART.R
+++ BART.R
@@ -1,3 +1,4 @@
+options(java.parameters = c("-XX:+UseConcMarkSweepGC", "-Xmx8192m"))
 library(bartMachine)
 library(dplyr)
 
@@ -11,9 +12,7 @@ train <- train %>% mutate(
   log_total_enrollment = log(total_enrollment)
 )
 
-options(java.parameters = "-Xmx8000m")
-set_bart_machine_num_cores(4)
-bartMod <- bartMachineCV(X = as.data.frame(train %>% dplyr::select(
+bartModFinal <- bartMachineCV(X = as.data.frame(train %>% dplyr::select(
   degree_length, room_and_board, out_of_state_tuition, 
   make_world_better_percent, stem_percent, degree_type, 
   type_school, ST_FIPS, locale, hbcu, annhi, AANAPII, his,
@@ -23,5 +22,14 @@ bartMod <- bartMachineCV(X = as.data.frame(train %>% dplyr::select(
   graduation_rate, age_entry, first_gen, pct_grad_degree, pct_domestic, 
   pct_dom_fth, unemployment_rate, Women, AmericanIndian, Asian, 
   Hispanic, PacificIslander, White, TwoOrMoreRaces, Unknown,
-  -early_career_pay)), y = log(train$early_career_pay)
+  -early_career_pay)), y = log(train$early_career_pay),
+  serialize = TRUE, num_burn_in = 1000, 
+  num_iterations_after_burn_in = 4000
   )
+
+save(bartModFinal, file = "./data/bartModFinal.RData")
+
+bartModFinal$cv_stats
+sort(bartMachine::get_var_counts_over_chain(bartModFinal))
+bartMachine::investigate_var_importance(bartModFinal)
+
