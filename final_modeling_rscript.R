7a8785b008581365c806cb9fa1ed6a6a13d9500d
connor-mcneill <mcneill.61@osu.edu>
2024-04-20 15:45:19 -0400

    update corrplot

--- final_modeling_rscript.R
+++ final_modeling_rscript.R
@@ -19,13 +19,17 @@ load('data/test.RData')
 # EDA ----
 ## Fit base linear model with multicollinearity fixed
 base.model <- lm(early_career_pay ~ . -name-state-mid_career_pay-state_code
-             -`Total Minority`-out_of_state_total-`Non-Resident Foreign`
-             -in_state_total-region-Black-in_state_tuition-rank-poverty_rate
-             -pred_degree_type-highest_degree, 
+             -`Total Minority`-out_of_state_total-`Non-Resident Foreign`, 
              data = train)
-summary(base.model)
 vifslm1 <- vif(base.model)
-vifslm1[order(vifslm1[,1],decreasing = T),]
+vifs.base = vifslm1[order(vifslm1[,1],decreasing = T),]
+vifs.names = rownames(vifs.base)
+rownames(vifs.base) = NULL
+vifs.to.print = data.frame(Predictor = vifs.names,
+                           VIF = round(vifs.base[,1], 3))
+library(xtable)
+print(xtable(vifs.to.print[vifs.to.print$VIF > 10,]),
+      booktabs=TRUE, include.rownames=FALSE)
 
 ## determine response transformation
 bc <- boxCox(base.model)
@@ -61,13 +65,26 @@ summary(transformed.lm)
 
 model.formula = as.formula(transformed.lm)
 
+plot(rstudent(transformed.lm) ~ hatvalues(transformed.lm))
+car::influencePlot(transformed.lm)
+car::outlierTest(transformed.lm, cutoff=.05, n.max=Inf)
+
+model.without.outliers = lm(model.formula,
+                            data=train[-c(444), ])
+summary(model.without.outliers)
+plot(rstudent(model.without.outliers) ~ hatvalues(model.without.outliers))
+car::influencePlot(model.without.outliers)
+car::outlierTest(model.without.outliers, cutoff=.05, n.max=Inf)
+train.without.outliers = train[-c(444),]
+
+
 # Linear Regression with Stepwise Regression ----
 ## set seed and number of groups for CV
 set.seed(704)
 K = 10
 ## fit stepwise model using train function 
 stepwise.selection <- train(model.formula, 
-                            data=train,
+                            data=train.without.outliers,
                             method='leapSeq',
                             tuneGrid=data.frame(nvmax = 1:50),
                             trControl=trainControl(method='cv',
@@ -78,11 +95,11 @@ stepwise.selection <- train(model.formula,
 stepwise.selection ## Model selected has six variables
 ## Get the final model from stepwise
 coef(stepwise.selection$finalModel, id=6)
-stepwise.model = lm(log(early_career_pay) ~ make_world_better_percent + 
+stepwise.model = lm(log(early_career_pay) ~ sat_avg + 
                       stem_percent +
                       tuit_fte + avg_fac_salary + pct_pell + pct_domestic + 
                       I(pct_domestic^4),
-                    data=train)
+                    data=train.without.outliers)
 summary(stepwise.model)
 ## Verify that our model meets linear regression assumptions
 plot(stepwise.model)
@@ -92,8 +109,9 @@ stepwise.rmsep = RMSE(stepwise.preds, log(test$early_career_pay))
 
 # Fit LASSO Model ----
 ## Fit the lasso model
-X.lasso = model.matrix(transformed.lm)[,-1]
-fit.lasso <- glmnet(X.lasso, log(train$early_career_pay), alpha=1)
+X.lasso = model.matrix(model.without.outliers)[,-1]
+fit.lasso <- glmnet(X.lasso, log(train.without.outliers$early_career_pay), 
+                    alpha=1)
 #plot(fit.lasso, label=TRUE, xvar='lambda')
 lambda.lasso = fit.lasso$lambda
 betahat.lasso = fit.lasso$beta
@@ -108,10 +126,12 @@ matplot(lambda.lasso, t(betahat.lasso * Xsd), type="l",
 axis(3, at=lambda.lasso, labels=fit.lasso$df)
 # perform CV to find optimal lambda
 set.seed(704)
-cv.lasso <- cv.glmnet(X.lasso, log(train$early_career_pay), alpha=1)
+cv.lasso <- cv.glmnet(X.lasso, log(train.without.outliers$early_career_pay), 
+                      alpha=1)
 plot(cv.lasso)
 lambda.lasso.chosen <- cv.lasso$lambda.1se
 coef(fit.lasso, s=lambda.lasso.chosen)
+coef(cv.lasso$glmnet.fit, s=lambda.lasso.chosen)
 X.test.lasso = model.matrix(model.formula, data=test)[,-1]
 lasso.preds = predict(fit.lasso, s=lambda.lasso.chosen, newx=X.test.lasso)
 lasso.rmsep = RMSE(lasso.preds, log(test$early_career_pay))
@@ -119,7 +139,7 @@ lasso.rmsep = RMSE(lasso.preds, log(test$early_career_pay))
 # Fit PLS Model ----
 set.seed(704)
 fit.pls = plsr(model.formula,
-     data=train,
+     data=train.without.outliers,
      scale=TRUE,
      validation = "CV",
      segments=10,
